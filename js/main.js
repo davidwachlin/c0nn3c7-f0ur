b5b172a424fb6793cc6f4d039c4c6b7b2fc7e693
David Wachlin <davidwachlin@gmail.com>
2019-06-13 15:58:00 -0400

    divs in grid layout, divs populate with row and col ids

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-$(document).ready( () => {
+$(document).ready(() => {
+
+    createBoard();
+
+    function createBoard() {
+        for (let col = 0; col < 7; col++) {
+            for (let row = 0; row < 6; row++) {
+                $(`<div class="game-block" id="col-${col} row-${row}"></div>`).appendTo($('#game-board'));
+                console.log(`col-${col} row-${row} `)
+            }
+        
+        }
+    }
+
+
+
+    /*
     
+    function for player taking turn
+    
+    each win condition is a function that evaluates to boolean
+    
+    each win function is called at the turn 
+    
+    if (win condition 1 ()|| win condition 2)
+    
+    */
+
 
 
 
